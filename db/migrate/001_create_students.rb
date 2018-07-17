f06c6034adaeaf1e3eea0c30628bae07aa2c2091
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-07-17 19:33:48 +0000

    automatically backed up by learn

--- db/migrate/001_create_students.rb
+++ db/migrate/001_create_students.rb
@@ -1,1 +1,1 @@
-class CreateStudents < ActiveRecord::Migrationdef changecreate_table :posts do |t|t.string :titlet.text :descriptionendendend
+class CreateStudents < ActiveRecord::Migration  def change    create_table :students do |t|      t.string :first_name      t.string :last_name    end  endend
